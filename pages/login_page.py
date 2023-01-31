1b9c718b9aef08bfb263d1b7aedd4d5a02feaae3
Dmytro Peretiatko <d.peretiatko@gmail.com>
2023-02-01 00:09:39 +0100

    third_task

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,7 +4,19 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id='password']"
-    sign_in_button_xpath ="//*[@id='__next']/form/div/div[2]/button/span[1]"
+    sign_in_button_xpath = "//*[text() = 'Sign in']"
+    login_url = ('https://scouts-test.futbolkolektyw.pl/en')
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
+
+    def type_in_password(self, password):
+        self.field_send_keys(self.login_field_xpath, password)
+
+    def click_on_the_sign_in_button(self):
+        self.field_send_keys(self.sign_in_button_xpath)
+
+    def title_of_page(self):
+        assert self.get_page_title(self.login_url) == self.expected_title
+
+
